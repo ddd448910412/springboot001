6e70d348b28473140b7657059ac95e581944241f
yijie <10412757@qq.com>
2019-03-10 13:40:00 +0800

    Initial commit

---
+++ src/main/java/com/yijie/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.yijie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.applet.Main;
+
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorld.class,args);
+    }
+}
